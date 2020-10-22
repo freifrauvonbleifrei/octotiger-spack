07091b5542efa5d3fbc2d4a90a42178f430aa8be
Gregor Daiß <Gregor.Daiss+git@gmail.com>
2020-10-22 17:47:39 +0200

    Added counters variant

--- octotiger/packages/cppuddle/package.py
+++ octotiger/packages/cppuddle/package.py
@@ -5,7 +5,13 @@ class Cppuddle(CMakePackage):
     version('master', git='https://github.com/G-071/hpx-kokkos-interopt-WIP.git',
         branch='master')
 
+    variant('counters', default=False, description='Build with allocation counters enabled.')
+
     def cmake_args(self):
         spec = self.spec
         args = []
+
+        args += [
+            self.define_from_variant('CPPUDDLE_WITH_COUNTERS', 'counters'),
+        ]
         return args
